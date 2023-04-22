97e92edd5d91dc0d3f61f3a3c7cb8f6b140de690
Lorenz <estre069@umn.edu>
2023-04-22 16:00:46 -0500

    initial implementation of DataCollection. still need integration

--- libs/transit/include/DataCollection.h
+++ libs/transit/include/DataCollection.h
@@ -1,29 +1,43 @@
 #ifndef DATA_COLLECTION_H_
 #define DATA_COLLECTION_H_
 
+/* Includes */
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <chrono>
+
+using namespace std;
+
 /**
  * @brief this class is responsible for collecting the data
  */
 class DataCollection {
     public:
-    // TODO: What do I need to add here to make this work?
-    // Create an object of DataCollection?
+        static DataCollection& GetInstance() {
+            static DataCollection instance;
+            return instance;
+        }
 
-    DataCollection() {
-    // TODO: Should I initialize the variables here so that the object gets updated with the simulation?
-    startTime = 0.0;
-    endTime = 0.0;
-    strategyUsed = "";
+        // Add data to collection
+        void AddData(string strategy, float fuel) {}
+        // Write data to file
+        void WriteDataToFile(string filename) {}
 
-    }
-    ~DataCollection();
+    private:
+        /* Private constructor and destructor to prevent instantiation */
+        DataCollection() {
+            start_time = chrono::high_resolution_clock::now(); 
+        }
+        ~DataCollection() {}
 
-    protected:
-    
-    double startTime;
-    double endTime;
-    std::string strategyUsed;
+        /* Private data members */
+        vector<string> strategies;
+        vector<float> fuels;
+        vector<float> speeds;
+        chrono::high_resolution_clock::time_point start_time;
+
 };
 
-
+#endif
 
--- libs/transit/src/DataCollection.cc
+++ libs/transit/src/DataCollection.cc
@@ -1,10 +1,30 @@
-// I have no idea how I'm going to implement this quite yet.
+/* DATA COLLECTION IMPLEMENTATION */
 
-void DataCollection::CollectData() {
-    std::ofstream myfile;
-    myfile.open("data.csv");
-    
-    myfile << "Strategy used, Time at pickup, Time at dropoff, Total time, Total battery used, Total battery remaining" << std::endl;
+/* Includes */
+#include "DataCollection.h"
 
+/* namespace */
+using namespace std;
 
+void DataCollection::AddData(string strategy, float fuel) {
+    strategies.push_back(strategy);
+    fuels.push_back(fuel);
+    auto end_time = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
+    speeds.push_back(static_cast<float>(duration.count())/1000);
 }
+
+void DataCollection::WriteDataToFile(string filename) {
+    ofstream file(filename);
+    if (file.is_open()) {
+        file << "Strategy,Speed (ms),Fuel" << endl;
+        for (int i = 0; i < strategies.size(); i++) {
+            file << strategies[i] << "," << speeds[i] << "," << fuels[i] << endl;
+        }
+        file.close();
+        cout << "Data written to file: " << filename << endl;
+    } else {
+        cout << "Unable to open file: " << filename << endl;
+    }
+}
+        
